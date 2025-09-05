b67a012ab344e735bfe99034253818c7ad1ac11b
Angela J Holden <angelajholden@gmail.com>
2025-09-05 12:24:16 -0500

    updating sandbox repo

---
+++ reels/reels.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body");
+const buttons = document.querySelectorAll(".menu_button");
+const open = document.querySelector(".open_button");
+const nav = document.querySelector(".navigation");
+
+function toggleMenu() {
+	buttons.forEach((button) => {
+		button.addEventListener("click", () => {
+			nav.classList.toggle("active");
+			const isActive = body.classList.toggle("menu_active");
+			if (isActive) {
+				open.setAttribute("aria-expanded", "true");
+			} else {
+				open.setAttribute("aria-expanded", "false");
+			}
+		});
+	});
+}
+
+function escapeMenu() {
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "Escape" && body.classList.contains("menu_active")) {
+			body.classList.remove("menu_active");
+			open.setAttribute("aria-expanded", "false");
+		}
+	});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	toggleMenu();
+	escapeMenu();
+});
